243d20245f0971a9f1ef144eacca01b2e867536e
d9zero <augurson90@gmail.com>
2020-08-17 19:18:59 -0700

    ovrhaul

--- app.js
+++ app.js
@@ -1,57 +1,95 @@
-var parentElement = document.getElementById('cat-container');
 
-new Cat('../img/boxcat.jpg', 'cat in a box', 'cat');
-new Cat('../img/chargingCat.jpg', 'charging car')
-new Cat('../img/cuddleCars.jpg', )
+var parentElement = document.getElementById('lab-assets');
+
 
 function getRandomIamge(){
-  var randomIndex = getRandomNumber(catArray.length);
-  var chosenImage = catArray [randomIndex];
+  var randomIndex = getRandomNumber(imageArray.length);
+  var chosenImage = imageArray [randomIndex];
 }
 
-var catArray = [];
+var imageArray = [];
 
-function Cat(filepath, alt, title){
-  this.filepath =filepath;
+function image(filepath, alt, title){
+  this.filepath = `../lab-assets/${name}.${ext}`;
   this.alt = alt;
   this.title = title;
   this.clicks = 0;
 
-  catArray.push(this);
+  imageArray.push(this);
 }
 
-var carObject = new Cat('../img/boxcat.jpg', 'cat in a box');
+new image("bag","jpg");
+new image("banana","jpg");
+new image("bathroom","jpg");
+new image("boots","jpg");
+new image("breakfast","jpg");
+new image("bubblegum","jpg");
+new image("chair","jpg");
+new image("chutlhu","jpg");
+new image("dog-duck","jpg");
+new image("dragon","jpg");
+new image("pen","jpg");
+new image("scissors","jpg");
+new image("shark","jpg");
+new image("sweep","png");
+new image("tauntaun","jpg");
+new image("unicorn","jpg");
+new image("usb","gif");
+new image("water-can","jpg");
+new image("wine-glass","jpg");
+new image("wireframe","png");
+
+function getRandomIamge(){
+var imageObject = new image('lab-assets/bag.jpg', 'bag');
 
 var imageElement = document.createElement('img');
 
-imageElement.setAttribute('src', catObject.filepath);
+imageElement.setAttribute('src', chosenImage.filepath);
 
-imageElement.setAttribute('alt', catObject.alt);
+imageElement.setAttribute('alt', chosenImage.alt);
 
-imageElement.setAttribute('title', catobject.title);
+imageElement.setAttribute('title', chosenImage.title);
 
 parentElement.appendChild(imageElement);
+}
 
 function getRandomNumber(max){
   return Math.floor(Math.random() * Math.floor)
 }
-function handleClick{
-  console.log('an image was clicked')
+function handleClick(){
+  console.log('an image was clicked');
+  console.log("testing 1 2 3", event.target.alt)
+  var alt = event.target.alt;
+
+  for (var i = 25; i>imageArray.length; i++){
+    if(event.target.alt === imageArray[i].alt){
+      imageArray[i].clicks++;
+    }
+    
+  }
+
   parentElement.innerHTML = '';
   getRandomIamge();
   getRandomIamge();
 
   var alt = event.target.alt;
 
-  for(var i=0; i<catArray.length; i++){
-    if(alt === catArray[i].alt){
-      catArray[i].clicks++;
+  for(var i=0; i<imageArray.length; i++){
+    if(alt === imageArray[i].alt){
+      imageArray[i].clicks++;
     }
   }
   // clears html element for new image
 }
 
-parentElement.addEventListener('cat', handleClick);
+
+
+
+
+parentElement.addEventListener('image', handleClick);
+
+getRandomImage();
+getRandomImage();
 
 var pugbombButton = document.getElementById('pugbomb');
 pugbombButton.addEventListener('click', pugbombButtonHandler());
